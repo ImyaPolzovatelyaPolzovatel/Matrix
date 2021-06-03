ff906d553e9eb2a310569abe1c026e56e3e4bc56
ImyaPolzovatelyaPolzovatel <78893996+ImyaPolzovatelyaPolzovatel@users.noreply.github.com>
2021-06-03 14:34:41 +1000

    Update dinoPRO.c

--- dinoPRO.c
+++ dinoPRO.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 //Справка 
 void printhelp(){
-    printf("Cправка\n\n\n"); 
+    printf("Cправка\n\n\n") 
     printf("Рабочий вызов: \n");
     printf("dinoPRO -c <имя файла> -f <номер фрагмента> -n <номер операции> \n\n");
 	printf("Вызов справки: \n");
